4ca1a19c53818abaa4e92af4151bddb282936bbd
zankowitch alexis <zankowitch@gmail.com>
2015-10-23 11:55:32 +0200

    add sql script creation bonus table
    autoincrement = easier to write find method

---
+++ tests/TestMatriceAdjacence.py
@@ -0,0 +1,14 @@
+from classes import MatriceAdjacence
+
+__author__ = 'azank'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        MatriceAdjacence
+
+
+if __name__ == '__main__':
+    unittest.main()
